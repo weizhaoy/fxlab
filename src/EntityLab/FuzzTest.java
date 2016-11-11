3657c7063f7b9bb8f055f383093112e6d0ea5414
weizhaoy <weizhaoy@163.com>
2016-11-11 17:07:23 +0800

    random run remains to be finished

--- src/EntityLab/FuzzTest.java
+++ src/EntityLab/FuzzTest.java
@@ -1,22 +1,29 @@
 package EntityLab;
 
 import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Created by weizhaoy on 16/11/2.
  */
 public class FuzzTest {
-    private static List<Seed> seeds;
-    private static Operation operation;
-    private static int SEED_TOTAL;
-    private static int OP_TOTAL;
-    private static double METRIC;/**Metric, double or others?**/
+    private  List<Seed> seeds;
+    private  Operation operation;
+    private  int SEED_TOTAL;
+    private  int OP_TOTAL;
+    private  double METRIC;
+    /**
+     * Metric, double or others?
+     **/
+    private static String STRATEGY = "Random";
     private static int ITER_NUM;//Times of Iterations
 
-    public FuzzTest(List<File> seedFiles, File opInterFile, List<File> opImplFiles){
+    public FuzzTest(List<File> seedFiles, File opInterFile, List<File> opImplFiles) {
         seeds = new ArrayList<Seed>();
         for (File seedFile : seedFiles) {
             seeds.add(new Seed(seedFile));
@@ -29,45 +36,122 @@ public class FuzzTest {
         ITER_NUM = 0;
     }
 
-    public void addSeed(File seedFile){
+    public void addSeed(File seedFile) {
         seeds.add(new Seed(seedFile));
     }
 
-    public static List<Seed> getSeeds() {
+    public  List<Seed> getSeeds() {
         return seeds;
     }
 
-    public static void setSeeds(List<Seed> seeds) {
-        FuzzTest.seeds = seeds;
+    public  void setSeeds(List<Seed> seeds) {
+        this.seeds = seeds;
     }
 
-    public static Operation getOperation() {
+    public  Operation getOperation() {
         return operation;
     }
 
-    public static void setOperation(Operation operation) {
-        FuzzTest.operation = operation;
+    public  void setOperation(Operation operation) {
+        this.operation = operation;
     }
 
-    public static int getSeedTotal() {
+    public  int getSeedTotal() {
         return SEED_TOTAL;
     }
 
-    public static void setSeedTotal(int seedTotal) {
+    public  void setSeedTotal(int seedTotal) {
         SEED_TOTAL = seedTotal;
     }
 
-    public static int getOpTotal() {
+    public  int getOpTotal() {
         return OP_TOTAL;
     }
 
-    public static void setOpTotal(int opTotal) {
+    public  void setOpTotal(int opTotal) {
         OP_TOTAL = opTotal;
     }
 
     public void run() {
         System.out.println("FuzzTest Run!");
+        ITER_NUM += 1;
+
+        OpImplementation opImplementation;
+        Method opMethod;
+        Seed[] seeds;
+        File[] seedFiles;
+        if (STRATEGY.equals("XXXX")) {
+
+        } else {
+            /**Default: Random**/
+            opImplementation = getRandomOpImpl();
+            opMethod = getMethodFromOpImpl(opImplementation);
+            int seedNum = opMethod.getParameterCount();
+            seeds = getRandomSeed(seedNum);
+            seedFiles = getFilesFromSeeds(seeds);
+
+
+
+            try {
+                Object opImplementationInstance = opImplementation.getOpClass().newInstance();//create instance
+                File newSeedFile = (File) opMethod.invoke(opImplementationInstance,seedFiles);//invoke method
+                if(newSeedFile.createNewFile()){
+                    System.out.println("New Seed "+newSeedFile.getAbsolutePath()+" generated.");
+                }else{
+                    System.out.println("New Seed "+newSeedFile.getAbsolutePath()+" failed.");
+                }
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
     }
+
+    private File[] getFilesFromSeeds(Seed[] seeds) {
+        List<File> files = new ArrayList<File>();
+        for (Seed seed : seeds) {
+            /**increase seed frequency number by one**/
+            seed.increaseTimesUsed();
+            files.add(seed.getSeedFile());
+        }
+        return (File[]) files.toArray();
+    }
+
+    private Method getMethodFromOpImpl(OpImplementation opImplementation) {
+        //todo: currently select the first one
+
+        /**increase opImplementation frequency number by one**/
+        opImplementation.increaseTimesUsed();
+
+        return opImplementation.getOpMethods().get(0);
+    }
+
+    private Seed[] getRandomSeed(int seedNum) {
+        List<Seed> seeds = new ArrayList<Seed>();
+        if(seedNum <= this.seeds.size()){
+            //todo: select seeds
+        }else{
+            seeds.addAll(this.seeds);
+            Collections.addAll(seeds, getRandomSeed(seedNum-this.seeds.size()));
+        }
+        return (Seed[]) seeds.toArray();
+    }
+
+    private OpImplementation getRandomOpImpl() {
+        List<OpImplementation> opImpls = this.operation.getOpImplementations();
+        //todo: select OpImplementation
+        return null;
+    }
+
+    
 
 
 }
--- src/EntityLab/Operation.java
+++ src/EntityLab/Operation.java
@@ -11,6 +11,22 @@ public class Operation {
     private OpInterface opInterface;
     private List<OpImplementation> opImplementations;
 
+    public OpInterface getOpInterface() {
+        return opInterface;
+    }
+
+    public void setOpInterface(OpInterface opInterface) {
+        this.opInterface = opInterface;
+    }
+
+    public List<OpImplementation> getOpImplementations() {
+        return opImplementations;
+    }
+
+    public void setOpImplementations(List<OpImplementation> opImplementations) {
+        this.opImplementations = opImplementations;
+    }
+
     public Operation(File opInterFile, List<File> opImplFiles){
         opInterface = new OpInterface(opInterFile);
         opImplementations = new ArrayList<OpImplementation>();
